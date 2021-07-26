b30a40f79d4d6d22cdb485a78b3da32a1981aae8
Kara LeSueur <klesueur@kornerstonecredit.com>
2021-07-26 15:36:15 -0600

    set up complete and tasks saving to state in console

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodoList([ todo, ...todoList ])
+  };
+
   return (
     <div className="App">
+      {console.log('todoList', todoList)}
       <header className="App-header">
         react todo
       </header>
+      <Form addTodo={addTodo} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'; 
 
 
-const Form = () => {
+const Form = ({ addTodo }) => {
+    const [todo, setTodo] = useState({
+        id: "",
+        task: "",
+        completed: false
+    });
+
+    const handleTaskInput = (e) => {
+        setTodo({ ...todo, task: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (todo.task.trim()) {
+            addTodo({ ...todo, id: uuidv4() })
+        }
+        setTodo({ ...todo, task: "" })
+    }
 
     return(
         <form>
-            <input />
-            <button />
+            {console.log('e.target.value setTodo', todo)}
+            <input
+                name="task"
+                type="text"
+                value={todo.task}
+                onChange={handleTaskInput} />
+            <button type="submit" onClick={handleSubmit} />
         </form>
     )
 };
