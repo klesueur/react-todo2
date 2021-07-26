048feafe65320e33c7ed9088398c7ba8af07af89
Kara LeSueur <klesueur@kornerstonecredit.com>
2021-07-26 13:41:17 -0600

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Form = () => {
+
+    return(
+        <form>
+            <input />
+            <button />
+        </form>
+    )
+};
+
+export default Form;
